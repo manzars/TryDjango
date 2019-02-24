7c49b48c05b05b26ed75be22c487c3287d32367c
manzars <manzarshaikh69@gmail.com>
2019-02-24 23:21:07 +0530

    Repo Updated

---
+++ trydjango/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>Hello World!</h1>")
+    return render(request, "home.html", {})
+
+def contacts_view(request, *args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1>Contact Pages!</h1>")
