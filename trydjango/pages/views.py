2f55594eac43534715bb61cef9fd38096f353c38
manzars <manzarshaikh69@gmail.com>
2019-02-28 01:40:48 +0530

    Day 3

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World!</h1>")
-    return render(request, "home.html", {})
+    my_contex = {
+        "my_name": "manzar",
+        "my_number": 9167121589,
+        "my_age": [123,564,656,76454,1416, 'abc']
+    }
+    return render(request, "home.html", my_contex)
 
 def contacts_view(request, *args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Contact Pages!</h1>")
+    #return HttpResponse("<h1>Contact Pages!</h1>")
+    
+    return render(request, "contact.html", {})
--- trydjango/product/views.py
+++ trydjango/product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
+def product_view(request):
+    obj = Products.objects.get(id = 1)
+    contex = {
+        "object": obj
+    }
+    return render(request, "product/details.html", contex)
