104cd5e5a7e3f01b368efba94a79070e80d821ce
manzars <manzarshaikh69@gmail.com>
2019-03-04 20:54:47 +0530

    Day 4

--- trydjango/blog/views.py
+++ trydjango/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.views.generic import (
     CreateView,
-    DeleteView,
+    DetailView,
     ListView,
     UpdateView,
     DeleteView
@@ -13,3 +13,13 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
     queryset = Article.objects.all()
+    
+class ArticleDetailView(DetailView):
+    template_name = 'articles/article_detail.html'
+    queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        abc = self.kwargs.get('id')
+        print(abc)
+        print(type(get_object_or_404(Article, id = abc)))
+        return get_object_or_404(Article, id = abc)
